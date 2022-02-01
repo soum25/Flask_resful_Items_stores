b5ba49abd8799b2154e36813f797fd0fb5a88f61
soum25 <vafesoumahoro@gmail.com>
2022-02-01 13:19:27 +0100

    add database to the code

--- security.py
+++ security.py
@@ -1,19 +1,11 @@
 from user import User
 
-users = [
-    User(1, "vaf", "edx4d")
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
-
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_username(username)
     if user and user.password == password:
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_id(user_id)
--- user.py
+++ user.py
@@ -1,7 +1,44 @@
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
+import sqlite3
+
+
+
+class User():
+    def __init__(self, id_, username, password):
+        self.id = id_
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_username(cls,username):
+        connection  = sqlite3.connect('data.db')
+        cursor =  connection.cursor()
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,)) #single tuple (username,)
+        row = result.fetchone()
+
+        if row:
+            user = cls(*row)
+        else:
+            None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_id(cls,id_):
+        connection  = sqlite3.connect('data.db')
+        cursor =  connection.cursor()
+        query = "SELECT * FROM users WHERE id_=?"
+        result = cursor.execute(query, (id_,))
+        row = result.fetchone()
+
+        if row:
+            user = cls(*row)
+        else:
+            None
+
+        connection.close()
+        return user
+
 
         
